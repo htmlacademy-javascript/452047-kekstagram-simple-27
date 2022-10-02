5b63a830589dd9bc5895e469ef6ff6775b5e631a
Boris Bakhtin <borisbakhtin@yandex.ru>
2022-10-03 02:48:15 +0300

    Функции 2го задания

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Рандомное целое число из заданного интервала
+
+function getRandomNumber(min, max) {
+  if (max <= min) {
+    [min, max] = [max, min];
+  }
+  if (min <= 0) {
+    min = 0;
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+getRandomNumber(3, 77);
+
+// Функция минимальной и максимальной длины строки
+
+function isStringValid(string, minLength, maxLength) {
+  return string.length >= minLength && maxLength >= string.length;
+}
+
+isStringValid('Проверочный текст комментария', 20, 140);
