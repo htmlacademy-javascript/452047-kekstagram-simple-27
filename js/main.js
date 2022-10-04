8e40b8e9d42371a96143ea3369953c4637d1d8e8
Boris Bakhtin <borisbakhtin@yandex.ru>
2022-10-04 14:45:25 +0300

    Исправление по 2му заданию

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ function getRandomNumber(min, max) {
   if (max <= min) {
     [min, max] = [max, min];
   }
-  if (min <= 0) {
-    min = 0;
+  if (min < 0 || max < 0) {
+    return NaN;
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
