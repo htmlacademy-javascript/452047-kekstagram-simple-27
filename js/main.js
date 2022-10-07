08874b3785345af3d4464dcd4c343754c2082a37
Boris Bakhtin <borisbakhtin@yandex.ru>
2022-10-07 12:44:57 +0300

    4-е задание Больше деталей

--- js/main.js
+++ js/main.js
@@ -19,3 +19,48 @@ function isStringValid(string, minLength, maxLength) {
 }
 
 isStringValid('Проверочный текст комментария', 20, 140);
+
+// Создание массива из 25 сгенерированных объектов
+
+const PHOTO_COUNT = 25;
+const DESCRIPTIONS = [
+  'Пристегнуть ремни',
+  'Бесконечность не предел',
+  'Назови мое имя',
+  'Опять работать',
+  'Газ до отказа',
+  'Сделал мой день',
+  'Повезло...',
+  'Завалил горизонт',
+  'Хороший, плохой, чудной',
+  'I am Batman!',
+  'Вечеринка в самом разгаре',
+  'Пришла посылка',
+  'После фотошопа',
+  'Это шедевр!',
+  'Смешное описание не придумал',
+];
+
+// Вернуть случайный элемент массива
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+// Функция создания фотографий и описания
+
+const createPhotos = () => {
+  const photos = [];
+
+  for (let index = 1; index <= PHOTO_COUNT; index++) {
+    photos.push({
+      id: index,
+      url: `photos/${index}.jpg`,
+      description: getRandomArrayElement(DESCRIPTIONS),
+      likes: getRandomNumber(15, 200),
+      comments: getRandomNumber(0, 200),
+    });
+  }
+
+  return photos;
+};
+
+createPhotos();
