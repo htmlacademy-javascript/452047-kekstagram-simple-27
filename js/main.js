58a9aeb04ca058f695f3474ea021c3d891343809
Boris Bakhtin <borisbakhtin@yandex.ru>
2022-10-12 09:55:24 +0300

    Исправления по заданиям 2 и 4

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
 // Рандомное целое число из заданного интервала
 
 function getRandomNumber(min, max) {
-  if (max <= min) {
-    [min, max] = [max, min];
-  }
-  if (min < 0 || max < 0) {
+  if (typeof min !== 'number') { // проверка на число
     return NaN;
   }
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  if (!Number.isFinite(min)) { // проверка на завершенность числа
+    return NaN;
+  }
+  if (min < 0) { // проверка на положительное значение числа
+    return NaN;
+  }
+  if (typeof max !== 'number') {
+    return NaN;
+  }
+  if (!Number.isFinite(max)) {
+    return NaN;
+  }
+  if (max < 0) {
+    return NaN;
+  }
+  if (Math.abs(min - max) < 1) { // Проверка разницы чисел в модуле не меньше 1
+    return NaN;
+  }
+
+  const safeMin = Math.min(min, max);
+  const safeMax = Math.max(min, max);
+
+  const actualMin = Math.ceil(safeMin); // Округление к ближайшему большему целому с помощью Math.ceil,
+  const actualMax = Math.floor(safeMax); // Округление к ближайшему меньшему целому с помощью Math.floor,
+
+  const result = Math.random() * (actualMax - actualMin + 1) + actualMin; // складываем дельту с мин. значением, чтобы получить итоговое случайное число.
+
+  return Math.floor(result); // Округляем результат (т.к. Math.random генерирует дробные числа от 0 до ~1)
 }
 
-getRandomNumber(3, 77);
+getRandomNumber(1, 3);
 
 // Функция минимальной и максимальной длины строки
 
@@ -43,17 +67,28 @@ const DESCRIPTIONS = [
 
 // Вернуть случайный элемент массива
 
-const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+const getRandomArrayElement = (elements) => {
+  if (!Array.isArray(elements)) { // Проверка на массив
+    return undefined;
+  }
+  if (elements.length === 0) { // Проверка на присутствие длины массива
+    return undefined;
+  }
+  if (elements.length === 1) { // Проверка на саму длину массива
+    return elements[0];
+  }
+  return elements[getRandomNumber(0, elements.length - 1)];
+};
 
 // Функция создания фотографий и описания
 
 const createPhotos = () => {
   const photos = [];
 
-  for (let index = 1; index <= PHOTO_COUNT; index++) {
+  for (let index = 0; index < PHOTO_COUNT; index++) {
     photos.push({
-      id: index,
-      url: `photos/${index}.jpg`,
+      id: index + 1,
+      url: `photos/${index + 1}.jpg`,
       description: getRandomArrayElement(DESCRIPTIONS),
       likes: getRandomNumber(15, 200),
       comments: getRandomNumber(0, 200),
