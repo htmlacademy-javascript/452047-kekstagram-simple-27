7c04fe76918c5888a6df814353d32840454d931d
Boris Bakhtin <borisbakhtin@yandex.ru>
2022-11-05 18:12:13 +0300

    Добавление stylesheet для nouislider.css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {renderPhotos} from './render-photos.js';
 import './user-form.js';
-import {resetScale} from './photo-scales.js';
+import { resetScale } from './photo-scales.js';
+import { resetEffects } from './photo-effects.js';
 
 renderPhotos();
 resetScale();
+resetEffects();
